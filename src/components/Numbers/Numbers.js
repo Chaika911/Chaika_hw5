ac70164e9061462d0aac2570ea190f4236bbc658
Vitalii <chayka1316@gmail.com>
2023-04-09 14:33:16 +0300

    add propTypes check

--- src/components/Numbers/Numbers.js
+++ src/components/Numbers/Numbers.js
@@ -14,22 +14,31 @@ class Numbers extends React.Component {
         const name = e.target.name;
         const value = e.target.value;
 
-        this.setState({[name]: +value});
+        if(!isNaN(value)){
+
+            this.setState({[name]: +value});
+        }
     }
 
     render(){
         return(
             <div>
                 <form>
-                    <input type="number" name="num1" value={this.state.num1} onChange={this.handleChange.bind(this)} />
+                    <input type="text" name="num1" value={this.state.num1} onChange={this.handleChange.bind(this)} />
                     <p>Number1 * 0.5 : {this.state.num1 * 0.5}</p><br/>
-                    <input type="number" name="num2" value={this.state.num2} onChange={this.handleChange.bind(this)} />
+                    <input type="text" name="num2" value={this.state.num2} onChange={this.handleChange.bind(this)} />
                     <p>Number2 * 0.05 : {this.state.num2 * 0.05}</p><br/>
 
                 </form>
             </div>
         )
     }
+}
+
+
+Numbers.propTypes = {
+    num1: PropTypes.number,
+    num2: PropTypes.number
 }
 
 export default Numbers;
